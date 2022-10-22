865e657fcd4d37843002f8c88cc7e34da392c9d7
zhudun <2310153070@qq.com>
2022-10-22 23:11:45 +0800

    校验验证码

--- api-passenger/src/main/java/com/yz/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/yz/apipassenger/service/VerificationCodeService.java
@@ -1,10 +1,13 @@
 package com.yz.apipassenger.service;
 
 import com.yz.apipassenger.remote.ServiceVerificationcodeClient;
+import com.yz.internalcommon.constant.CommonStatusEnum;
 import com.yz.internalcommon.dto.ResponseResult;
 import com.yz.internalcommon.response.NumberCodeResponse;
 import com.yz.internalcommon.response.TokenResponse;
+import io.netty.util.internal.StringUtil;
 import net.sf.json.JSONObject;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -40,7 +43,7 @@ public class VerificationCodeService {
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
         //key,value,过期时间
-        String key = verificationCodePrefix + passengerPhone;
+        String key = generatorKeyByPhone(passengerPhone) ;
         //存入redis
         stringRedisTemplate.opsForValue().set(key, numberCode + "", 2, TimeUnit.MINUTES);
 
@@ -48,6 +51,15 @@ public class VerificationCodeService {
 
         //返回值
         return ResponseResult.success("");
+    }
+
+    /**
+     * 根据手机号生成key
+     * @param passengerPhone
+     * @return
+     */
+    private String generatorKeyByPhone(String passengerPhone){
+        return verificationCodePrefix + passengerPhone;
     }
 
     /**
@@ -59,10 +71,22 @@ public class VerificationCodeService {
      */
     public ResponseResult checkCode(String passengerPhone, String verificationCode) {
         //根据手机号，去redis读取验证码
-        System.out.println("据手机号，去redis读取验证码");
+        System.out.println("根据手机号，去redis读取验证码");
+
+        //生成key
+        String key = generatorKeyByPhone(passengerPhone) ;
+        //根据key获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的value = " + codeRedis);
 
         //校验验证码
-        System.out.println("校验验证码");
+        if(StringUtils.isBlank(codeRedis)){
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getMessage());
+        }
+        if(!verificationCode.trim().equals(codeRedis.trim())){
+            return ResponseResult.fail(CommonStatusEnum.VERIFICATION_CODE_ERROR.getCode(),CommonStatusEnum.VERIFICATION_CODE_ERROR.getMessage());
+
+        }
 
         //判断原来是否有用户，并进行对应的处理
         System.out.println("判断原来是否有用户，并进行对应的处理");
--- internal-common/src/main/java/com/yz/internalcommon/constant/CommonStatusEnum.java
+++ internal-common/src/main/java/com/yz/internalcommon/constant/CommonStatusEnum.java
@@ -10,7 +10,17 @@ import lombok.Getter;
  */
 @Getter
 public enum CommonStatusEnum {
+    /**
+     * 验证码错误提示：1000-1099
+     */
+    VERIFICATION_CODE_ERROR(1099,"验证码不正确"),
+    /**
+     * 成功
+     */
     SUCCESS(1,"success"),
+    /**
+     * 失败
+     */
     FAIL(0,"fail")
 
 
