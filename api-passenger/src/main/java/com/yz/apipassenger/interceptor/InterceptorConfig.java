34ee40f6de0718a3d8eb5d035d7809f9ea687003
zhudun <2310153070@qq.com>
2022-10-23 23:02:51 +0800

    token拦截器测试

---
+++ api-passenger/src/main/java/com/yz/apipassenger/interceptor/InterceptorConfig.java
@@ -0,0 +1,24 @@
+package com.yz.apipassenger.interceptor;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author: yangzhen
+ * @Date 2022/10/23-22:28
+ * @Description: com.yz.apipassenger.interceptor
+ * @version: 1.0
+ */
+@Configuration
+public class InterceptorConfig implements WebMvcConfigurer {
+
+    public void addInterceptors(InterceptorRegistry registry){
+        registry.addInterceptor(new JwtInterceptor())
+        //拦截的路径
+        .addPathPatterns("/**")
+        //不拦截的路径
+        .excludePathPatterns("/noauthTest");
+
+    }
+}
