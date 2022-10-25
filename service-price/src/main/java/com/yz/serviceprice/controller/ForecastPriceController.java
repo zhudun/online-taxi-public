066cb59f4a0c8387811ff2726ffaded2cea3985d
zhudun <2310153070@qq.com>
2022-10-25 17:07:24 +0800

    service-price 骨架编写完成

---
+++ service-price/src/main/java/com/yz/serviceprice/controller/ForecastPriceController.java
@@ -0,0 +1,36 @@
+package com.yz.serviceprice.controller;
+
+import com.yz.serviceprice.service.ForecastPriceService;
+import com.yz.internalcommon.dto.ResponseResult;
+import com.yz.internalcommon.request.ForeCastPriceDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+
+/**
+ * @Author: yangzhen
+ * @Date 2022/10/25-16:47
+ * @Description: com.yz.serviceprice.controller
+ * @version: 1.0
+ */
+@RestController
+public class ForecastPriceController {
+
+    @Autowired
+    private ForecastPriceService forecastPriceService;
+
+    @PostMapping("/forecast-price")
+    public ResponseResult forecastPrice(@RequestBody ForeCastPriceDTO foreCastPriceDTO){
+
+        String depLongitude = foreCastPriceDTO.getDepLongitude();
+        String depLatitude = foreCastPriceDTO.getDepLatitude();
+        String destLongitude = foreCastPriceDTO.getDestLongitude();
+        String destLatitude = foreCastPriceDTO.getDestLatitude();
+
+        return forecastPriceService.forecastPrice(depLongitude,depLatitude,destLongitude,destLatitude);
+    }
+}
---
+++ service-price/src/main/java/com/yz/serviceprice/service/ForecastPriceService.java
@@ -0,0 +1,48 @@
+package com.yz.serviceprice.service;
+
+import com.yz.internalcommon.dto.ResponseResult;
+import com.yz.internalcommon.response.ForecastPriceResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: yangzhen
+ * @Date 2022/10/25-15:58
+ * @Description: com.yz.apipassenger.service
+ * @version: 1.0
+ */
+@Service
+@Slf4j
+public class ForecastPriceService {
+
+    /**
+     * 根据出发地和目的地经纬度  计算预估价
+     * @param depLongitude
+     * @param depLatitude
+     * @param destLongitude
+     * @param destLatitude
+     * @return
+     */
+    public ResponseResult forecastPrice(String depLongitude,String depLatitude,String destLongitude,String destLatitude){
+
+        log.info("出发地的经度:"+ depLongitude);
+        log.info("出发地的纬度:"+ depLatitude);
+        log.info("到达地的经度:"+ destLongitude);
+        log.info("到达地的经度:"+ destLatitude);
+
+        log.info("调用地图服务，查询距离和时长");
+
+        log.info("调用计价规则");
+
+        log.info("根据距离，时长，计价规则，计算价格");
+
+
+        ForecastPriceResponse forecastPriceResponse = new ForecastPriceResponse();
+        forecastPriceResponse.setPrice(12.35);
+
+        return ResponseResult.success(forecastPriceResponse);
+
+    }
+
+
+}
