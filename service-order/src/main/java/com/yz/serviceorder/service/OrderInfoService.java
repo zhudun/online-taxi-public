33872009b758e8bf14c4d6cc383937a44738f68e
zhudun <2310153070@qq.com>
2022-12-24 22:01:32 +0800

    单机解决实时订单并发问题

--- service-order/src/main/java/com/yz/serviceorder/service/OrderInfoService.java
+++ service-order/src/main/java/com/yz/serviceorder/service/OrderInfoService.java
@@ -106,7 +106,7 @@ public class OrderInfoService {
      * 实时订单派单逻辑
      * @param orderInfo
      */
-    public void dispatchRealTimeOrder(OrderInfo orderInfo){
+    public synchronized void  dispatchRealTimeOrder(OrderInfo orderInfo){
 
         //2km
         String depLatitude = orderInfo.getDepLatitude();
