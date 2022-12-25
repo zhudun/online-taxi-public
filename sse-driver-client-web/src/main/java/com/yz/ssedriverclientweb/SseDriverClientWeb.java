25da3bdc3a6bff24c3b5aee53d2a578780a6be1e
zhudun <2310153070@qq.com>
2022-12-25 21:48:51 +0800

    模拟司机客户端的开发

---
+++ sse-driver-client-web/src/main/java/com/yz/ssedriverclientweb/SseDriverClientWeb.java
@@ -0,0 +1,18 @@
+package com.yz.ssedriverclientweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author: yangzhen
+ * @Date 2022/12/25-21:43
+ * @Description: com.yz.ssedriverclientweb
+ * @version: 1.0
+ */
+@SpringBootApplication
+public class SseDriverClientWeb {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SseDriverClientWeb.class,args);
+    }
+}
